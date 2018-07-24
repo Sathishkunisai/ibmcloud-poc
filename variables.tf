04ee9e1e45d79a1c5a367e89e4752f6538b6aa4e
sathish <sathish.k.ramaswamy@kp.org>
2018-07-24 16:34:44 -0700

    test

---
+++ variables.tf
@@ -0,0 +1,58 @@
+variable "app_version" {
+  default = "1"
+}
+
+variable "git_repo" {
+  default = "https://github.com/karthick9500/ibm-nodejs-sample.git"
+}
+
+variable "dir_to_clone" {
+  default = "/tmp/my_cf_code7"
+}
+
+variable "app_zip" {
+  default = "/tmp/node.zip"
+}
+
+variable "space_name" {
+  default = "dev"
+}
+
+
+variable "org_name" {
+  default = "sathishkumar.ramaswamy@cognizant.com"
+}
+
+
+
+variable "route_domain" {
+  default = "mybluemix.net"
+}
+
+variable "route" {
+  default = "nodejs-poc-kp"
+}
+
+variable "service_instance_name" {
+  default = "myservice"
+}
+
+variable "service_offering" {
+ default = "cloudantNoSQLDB"
+}
+
+variable "plan" {
+  default = "Lite"
+}
+
+variable "app_name" {
+  default = "nodejs-poc"
+}
+
+variable "command" {
+  default = ""
+}
+
+variable "buildpack" {
+  default = "https://github.com/cloudfoundry/nodejs-buildpack.git"
+}
